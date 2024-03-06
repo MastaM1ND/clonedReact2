9a38c2f522c6d0a1f7f9c48c3a9a1c854e7a0627
MastaM1ND <owenmbarry@gmail.com>
2024-03-05 19:55:21 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './navbar.js'; 
+import Card from './card.js';
+
+function App() {
+    return (
+        <div className="App">
+
+            < Navbar />
+
+            <h1><strong> CS 230 Lab </strong></h1>
+            <h2><strong> Section - 001 </strong></h2>
+            <p> WVU ID: 800358898 </p>
+            <p> Hi I am Stephanie. </p>
+            
+            < Card />
+
+        </div>
+    );
+}
+export default App;
